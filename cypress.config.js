c29d52a9fd86d924eccc9e2021142d9ee44df172
Nanncy Andonissamy <nanncy.andonissamy@sogeti.com>
2026-03-09 23:43:55 +0100

    MAJ fichiers

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,24 @@
-module.exports = {
-  e2e: {
-    setupNodeEvents(on, config) {
-      // pas besoin de changer ici
-    },
+//module.exports = {
+  //e2e: {
+    //setupNodeEvents(on, config) {
+     
+    //},
     
-    baseUrl: "http://127.0.0.1:8080"
-  }
-};
+    //baseUrl: "http://127.0.0.1:8080"
+  //}
+//};
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  reporter: 'mochawesome',
+  reporterOptions: {
+    reportDir: 'cypress/reports/mocha',
+    overwrite: false,
+    html: false,   // On génère le HTML via marge après la fusion
+    json: true,
+  },
+  e2e: {
+    baseUrl: 'http://localhost:8080',
+    setupNodeEvents(on, config) {},
+  },
+})
