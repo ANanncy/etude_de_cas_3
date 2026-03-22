245c63fc086f3e822876c8f1c2087667150e5917
Nanncy Andonissamy <nanncy.andonissamy@sogeti.com>
2026-03-22 17:40:40 +0100

    Récupérer package.json, package-lock.json, Cypress config et workflow depuis demo

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,13 @@
 //module.exports = {
   //e2e: {
     //setupNodeEvents(on, config) {
-     
+      // pas besoin de changer ici
     //},
     
     //baseUrl: "http://127.0.0.1:8080"
   //}
 //};
+
 const { defineConfig } = require('cypress')
 
 module.exports = defineConfig({
