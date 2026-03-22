0a6995083a16c7ab7ed794163104657263bcd83a
Nanncy Andonissamy <nanncy.andonissamy@sogeti.com>
2026-03-22 16:51:46 +0100

    Remplacer workflow, cypress.config.js et package.json par ceux de demo

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,25 @@
-module.exports = {
-  e2e: {
-    setupNodeEvents(on, config) {
-     
-    },
+//module.exports = {
+  //e2e: {
+    //setupNodeEvents(on, config) {
+      // pas besoin de changer ici
+    //},
     
-    baseUrl: "http://127.0.0.1:8080"
-  }
-};
+    //baseUrl: "http://127.0.0.1:8080"
+  //}
+//};
+
+const { defineConfig } = require('cypress')
+
+module.exports = defineConfig({
+  reporter: 'mochawesome',
+  reporterOptions: {
+    reportDir: 'cypress/reports/mocha',
+    overwrite: false,
+    html: false,   // On génère le HTML via marge après la fusion
+    json: true,
+  },
+  e2e: {
+    baseUrl: 'http://localhost:8080',
+    setupNodeEvents(on, config) {},
+  },
+})
